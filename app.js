4e4deba2ce598241615b7e5c1f41df2a5b50e471
cficht <chris.ficht@gmail.com>
2020-02-18 23:06:11 -0800

    making some progress

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require('express');
+// const darkSkyData = require('./data/darksky.json');
+const geoData = require('./data/geo.json');
 
 const app = express();
 
+const getLatGetLong = (number) => {
+    const data = geoData.results;
 
-app.get('/location', (request, response) => {
+    const dataMapped = data.map(address => {
+        const addresses = {};
+
+        addresses.formatted_query = address.formatted_address;
+        addresses.latitude = address.geometry.location.lat;
+        addresses.longitude = address.geometry.location.lng;
+        
+        return addresses;
+    });
+    return dataMapped[number];
+};
+
+app.get('/location/:validLoc', (request, response) => {
+    const mungLatAndLong = getLatGetLong(request.params.validLoc);
     response.json({
-        some: 'json'
+        formatted_query: mungLatAndLong.formatted_query,
+        latitude: mungLatAndLong.latitude,
+        longitude: mungLatAndLong.longitude,
     });
 });
 
