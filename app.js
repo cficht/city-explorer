1ec60811505b08bc42aa3d28f77983e3d07031c6
cficht <chris.ficht@gmail.com>
2020-02-19 14:55:39 -0800

    did weather

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const express = require('express');
-const darkSkyData = require('./data/darksky.json');
-const geoData = require('./data/geo.json');
+// const darkSkyData = require('./data/darksky.json');
+// const geoData = require('./data/geo.json');
 const cors = require('cors');
+const request = require('superagent');
 
 const app = express();
 app.use(cors());
@@ -9,66 +11,74 @@ app.use(cors());
 let latitude;
 let longitude;
 
-const getLatGetLong = (number) => {
-    const data = geoData.results;
-
-    const dataMapped = data.map(address => {
-        const addresses = {};
-
-        addresses.formatted_query = address.formatted_address;
-        addresses.latitude = address.geometry.location.lat;
-        addresses.longitude = address.geometry.location.lng;
-        
-        return addresses;
-    });
-    return dataMapped[number];
-};
-
-app.get('/location', (request, response) => {
-    const location = request.query.search;
-    console.log(location);
-
-    const mungLatAndLong = getLatGetLong(0);
-
-    latitude = mungLatAndLong.latitude;
-    longitude = mungLatAndLong.longitude;
-    console.log(latitude);
-    console.log(longitude);
-
-    response.json({
-        formatted_query: mungLatAndLong.formatted_query,
-        latitude: mungLatAndLong.latitude,
-        longitude: mungLatAndLong.longitude,
-    });
+app.get('/location', async(req, res, next) => {
+    try {
+        const location = req.query.search;
+        const URL = `https://us1.locationiq.com/v1/search.php?key=${process.env.GEOCODE_API_KEY}&q=${location}&format=json`;
+        const locationData = await request.get(URL);
+    
+        const firstResult = locationData.body[0];
+    
+        latitude = firstResult.lat;
+        longitude = firstResult.lon;
+    
+        res.json({
+            formatted_query: firstResult.display_name,
+            latitude: latitude,
+            longitude: longitude,
+        });
+    } catch (err) {
+        next(err);
+    }
 });
 
 
-const getWeatherData = (latitude, longitude) => {
-    return darkSkyData.daily.data.map(forecast => {
-        return {
-            forecast: forecast.summary,
-            time: new Date(forecast.time * 1000)
-        };
-    });
-};
+// const getWeatherData = (latitude, longitude) => {
+//     return darkSkyData.daily.data.map(forecast => {
+//         return {
+//             forecast: forecast.summary,
+//             time: new Date(forecast.time * 1000)
+//         };
+//     });
+// };
 
-app.get('/weather', (request, response) => {
-    // use the lat and long from earlier to get weather data for the selected area
-    const portlandWeather = getWeatherData(request.query.latitude, request.query.longitude);
+app.get('/weather', async(req, res, next) => {
+    try {
+        // latitude = 45.5234211;
+        // longitude = -122.6809008;
+        latitude = request.query.latitude;
+        longitude = request.query.longitude;
 
-    // res.js that weather data in the appropriate form
-    response.json(portlandWeather);
+        const URL = `https://api.darksky.net/forecast/${process.env.DARKSKY_API_KEY}/${latitude},${longitude}`;
+
+        const darkSkyData = await request.get(URL);
+        const weatherData = darkSkyData.body.daily;
+        const time = weatherData.data[0].time;
+        console.log(weatherData);
+
+        // const portlandWeather = getWeatherData(req.query.latitude, req.query.longitude);
+
+        res.json({
+            forecast: weatherData.summary,
+            time: new Date(time * 1000) });
+
+    } catch (err) {
+        next(err);
+    }
+
 });
 
-app.get('*', (request, response) => {
-    response.json({
+
+app.get('*', (req, res) => {
+    res.json({
         onNo: '404'
     });
 });
 
 // app.listen(3000, () => { console.log('running...'); });
 
-// console.log(getLatGetLong(0));
+console.log(latitude);
+console.log(longitude)
 
 module.exports = {
     app: app
